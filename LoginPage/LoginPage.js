3d1fd63f021c5ba75eec2ad04a91174dc1af89dc
Guy Gabriel Dziubas <dziubas@post.bgu.ac.il>
2026-01-18 20:53:39 +0200

    Delete LoginPage directory

--- LoginPage/LoginPage.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('loginForm');
-
-// מאזין לאירוע שליחת הטופס ומדפיס הודעה לקונסול (נועד לבדיקה, השליחה עצמה מתבצעת דרך ה-HTML)
-form.addEventListener('submit', function (event) {
-  console.log('submit handler עובד'); // בדיקה
-});
